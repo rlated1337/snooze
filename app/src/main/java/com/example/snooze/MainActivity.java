8dd98e9a717da5ce6f8640cf0fcc24ff96bede88
Huncho6 <azurman@live.com>
2019-06-07 11:57:17 +0200

    added libraries for list and call

--- app/src/main/java/com/example/snooze/MainActivity.java
+++ app/src/main/java/com/example/snooze/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.snooze;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         SnoozeUsersService service = retrofit.create(SnoozeUsersService.class);
-        Call<List<SnoozeUsers>> call = SnoozeUsersService.getAllUsers();
+        Call<List<SnoozeUsers>> call = service.getAllUsers();
 
 
     }
--- app/src/main/java/com/example/snooze/SnoozeUsersService.java
+++ app/src/main/java/com/example/snooze/SnoozeUsersService.java
@@ -1,5 +1,8 @@
 package com.example.snooze;
 
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface SnoozeUsersService {
