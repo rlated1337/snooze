33088031e66963d7ad9446a5f361a610da979871
rlated <milch44@googlemail.com>
2019-06-07 11:56:28 +0200

    full connection to API and example endpoint

--- app/src/main/java/com/example/snooze/MainActivity.java
+++ app/src/main/java/com/example/snooze/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.snooze;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
-    private static Retrofit retrofit;
+    private Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,36 @@ public class MainActivity extends AppCompatActivity {
 
         retrofit = connect.getRetrofitInstance();
 
-
-
         SnoozeUsersService service = retrofit.create(SnoozeUsersService.class);
-        Call<List<SnoozeUsers>> call = SnoozeUsersService.getAllUsers();
 
+        Call<List<SnoozeUsers>> call = service.getAllUsers();
+
+        call.enqueue(new Callback<List<SnoozeUsers>>() {
+            @Override
+            public void onResponse(Call<List<SnoozeUsers>> call, Response<List<SnoozeUsers>> response) {
+                    if(!response.isSuccessful()){
+                        System.out.println(response.code());
+                    }
+
+                    List<SnoozeUsers> users = response.body();
+
+                    for (SnoozeUsers user : users) {
+                        String content = "";
+
+                        content += "ID: " + user.getId() +  "\n";
+                        content += "Name: " + user.getUsername() + "\n";
+                        content += "Email: " + user.getEmail() + "\n";
+                        content += "Realm: " + user.getRealm() + "\n";
+                        content += "-----------";
+                        System.out.println(content);
+                    }
+            }
+
+            @Override
+            public void onFailure(Call<List<SnoozeUsers>> call, Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        });
 
     }
 }
