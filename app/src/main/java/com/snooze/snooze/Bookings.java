a2bdb5131ccd16e54e0e0f6596ab76a553f632f8
bluefire111 <Hariss99@gmx.de>
2019-06-19 19:48:34 +0200

    Bookings

--- app/src/main/java/com/snooze/snooze/Bookings.java
+++ app/src/main/java/com/snooze/snooze/Bookings.java
@@ -7,9 +7,12 @@ import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
-import com.snooze.api.snooze.inc.Capsules;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.snooze.api.snooze.inc.Bookings;
 import com.snooze.model.snooze.controller.AppController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bookings extends AppCompatActivity {
@@ -17,7 +20,7 @@ public class Bookings extends AppCompatActivity {
     private Button btn_history;
     private ScrollView sView_List;
     private AppController aController;
-    private List<String> sList_History;
+    private List<com.snooze.api.snooze.inc.Bookings> listBookings = new ArrayList<com.snooze.api.snooze.inc.Bookings>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +36,7 @@ public class Bookings extends AppCompatActivity {
         sView_List = findViewById(R.id.sView_Bookings_BookingList);
         aController = MainActivity.getInstance().getaController();
 
-
+        /*
         aController.getBookings();
 
         aController.setBookingListener(new AppController.DataInterfaceBookings() {
@@ -42,9 +45,20 @@ public class Bookings extends AppCompatActivity {
                 System.out.println(myBookings);
             }
         });
+        */
 
 
 
+
+    }
+    public void printBookingsList(){
+        for(com.snooze.api.snooze.inc.Bookings booking : listBookings){
+            String content = "";
+            content += "Name: " + booking.getDate() + "\n";
+            content += "Preis: " + booking.getAmount()  + "\n";
+            content += "________________________" + "\n";
+            System.out.println(content);
+        }
     }
 }
 
