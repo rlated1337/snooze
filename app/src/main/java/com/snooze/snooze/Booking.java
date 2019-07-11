3bf2c726f4ad272e7ccceba67c54566d6446395b
bluefire111 <Hariss99@gmx.de>
2019-07-11 23:20:06 +0200

    booking

--- app/src/main/java/com/snooze/snooze/Booking.java
+++ app/src/main/java/com/snooze/snooze/Booking.java
@@ -50,6 +50,8 @@ public class Booking extends AppCompatActivity {
     private List<String> listDate = new ArrayList<>();
     private List<String> listPeriod = new ArrayList<>();
     private List<String> listName = new ArrayList<>();
+    private List<String> listPin = new ArrayList<>();
+    private List<String> listTime = new ArrayList<>();
 
 
 
@@ -89,7 +91,7 @@ public class Booking extends AppCompatActivity {
 
     }
     public void buildRecyclerView(){
-    mAdapter = new BookingsAdapter(listAmount,listDate,listPeriod,listName,this);
+    mAdapter = new BookingsAdapter(listAmount,listDate,listPeriod,listName,this,listPin,listTime);
     mRecyclerView.setAdapter(mAdapter);
     mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -105,6 +107,7 @@ public class Booking extends AppCompatActivity {
 
         for(int i=0;i<jarray.size();i++){
             int firstTimeFrame,lastTimeFrame;
+            String pin;
             int countBookings;
             int period=0;
             String resultAmount,resultDate,resultName;
@@ -115,7 +118,8 @@ public class Booking extends AppCompatActivity {
             listDate.add(i,resultDate.substring(1, resultDate.indexOf('T')));
             firstTimeFrame = Integer.parseInt(jsonObject.get("FirstTimeFrame").toString());
             lastTimeFrame = Integer.parseInt(jsonObject.get("LastTimeFrame").toString());
-
+            pin = jsonObject.get("Pin").toString();
+            listPin.add(pin);
             JsonObject capsuleObject = jsonObject.getAsJsonObject("capsule");
             resultName = capsuleObject.get("Name").toString();
             resultName.substring(1);
@@ -125,26 +129,54 @@ public class Booking extends AppCompatActivity {
             System.out.println(resultName);
 
 
-            if (firstTimeFrame == lastTimeFrame)
-             {
-                countBookings = firstTimeFrame;
-             }
-            else
-            {
-                countBookings = lastTimeFrame-firstTimeFrame;
-            }
 
-            while(countBookings>0)
-            {
-                period = period + 20;
-                countBookings--;
-            }
 
             listPeriod.add(i,"20" + " minutes");
 
         }
     }
 
+    private void initTimeFrameMap() {
+        timeFrameTranslation = new SparseArray<>();
+
+        timeFrameTranslation.append(1, "09.00 bis 09.20 Uhr");
+        timeFrameTranslation.append(2, "09.20 bis 09.40 Uhr");
+        timeFrameTranslation.append(3, "09.40 bis 10.00Uhr");
+
+        timeFrameTranslation.append(4, "10.00 bis 10.20 Uhr");
+        timeFrameTranslation.append(5, "10.20 bis 10.40 Uhr");
+        timeFrameTranslation.append(6, "10.40 bis 11.00 Uhr");
+
+        timeFrameTranslation.append(7, "11.00 bis 11.20 Uhr");
+        timeFrameTranslation.append(8, "11.20 bis 11.40 Uhr");
+        timeFrameTranslation.append(9, "11.40 bis 12.00 Uhr");
+
+        timeFrameTranslation.append(10, "12.00 bis 12.20 Uhr");
+        timeFrameTranslation.append(11, "12.20 bis 12.40 Uhr");
+        timeFrameTranslation.append(12, "12.40 bis 13.00 Uhr");
+
+        timeFrameTranslation.append(13, "13.00 bis 13.20 Uhr");
+        timeFrameTranslation.append(14, "13.20 bis 13.40 Uhr");
+        timeFrameTranslation.append(15, "13.40 bis 14.00 Uhr");
+
+        timeFrameTranslation.append(16, "14.00 bis 14.20 Uhr");
+        timeFrameTranslation.append(17, "14.20 bis 14.40 Uhr");
+        timeFrameTranslation.append(18, "14.40 bis 15.00 Uhr");
+
+        timeFrameTranslation.append(19, "15.00 bis 15.20 Uhr");
+        timeFrameTranslation.append(20, "15.20 bis 15.40 Uhr");
+        timeFrameTranslation.append(21, "15.40 bis 16.00 Uhr");
+
+        timeFrameTranslation.append(22, "16.00 bis 16.20 Uhr");
+        timeFrameTranslation.append(23, "16.20 bis 16.40 Uhr");
+        timeFrameTranslation.append(24, "16.40 bis 17.00 Uhr");
+
+        timeFrameTranslation.append(25, "17.00 bis 17.20 Uhr");
+        timeFrameTranslation.append(26, "17.20 bis 17.40 Uhr");
+        timeFrameTranslation.append(27, "17.40 bis 18.00 Uhr");
+
+        Log.d("Time Frame Translation", timeFrameTranslation.toString());
+    }
 
     private static String removeLastChar(String str) {
         return str.substring(0, str.length() - 1);
